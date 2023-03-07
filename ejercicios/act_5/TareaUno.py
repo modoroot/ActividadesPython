cc92f20552422f93ba1837e19a4521ffa6e7cfef
amna <antoniomiguel2001@hotmail.es>
2023-03-07 23:55:45 +0100

    joyita

--- ejercicios/act_5/TareaUno.py
+++ ejercicios/act_5/TareaUno.py
@@ -1,26 +1,52 @@
-# Definición de las coordenadas del laberinto y los muros que lo componen (fila, columna)
 muro = ((0, 1), (0, 2), (0, 3), (0, 4), (1, 1), (2, 1), (2, 3), (3, 3), (4, 0), (4, 1), (4, 2), (4, 3))
 
-# Crea el laberinto a partir de las coordenadas de los muros y los espacios vacíos
 laberinto = []
 for fila in range(5):
-    # Crea una lista vacía para la fila actual del laberinto y la añade a la lista
     lista_fila = []
     for columna in range(5):
-        # Comprueba si la casilla actual está en la lista de muros
         if (fila, columna) in muro:
-            # Si hay muro, se añade una X a la lista
             lista_fila.append('X')
-        # Comprueba si la casilla actual es la de salida del laberinto
         elif (fila, columna) == (4, 4):
-            # Si es la casilla de salida, se añade una S a la lista
             lista_fila.append('S')
         else:
-            # Si no hay muro, se añade un espacio en blanco
             lista_fila.append(' ')
-    # Añadir la lista de la fila actual al laberinto
     laberinto.append(lista_fila)
 
-# Mostrar el laberinto por pantalla con los espacios entre cada casilla
+
+def resolver_laberinto(fila_actual, columna_actual, camino, visitados):
+    if (fila_actual, columna_actual) == (4, 4):
+        return camino
+
+    if fila_actual < 4 and (fila_actual + 1, columna_actual) not in muro and (
+            fila_actual + 1, columna_actual) not in visitados:
+        pila.append((fila_actual + 1, columna_actual, camino + ['Abajo']))
+
+    if columna_actual < 4 and (fila_actual, columna_actual + 1) not in muro and (
+            fila_actual, columna_actual + 1) not in visitados:
+        pila.append((fila_actual, columna_actual + 1, camino + ['Derecha']))
+
+    if fila_actual > 0 and (fila_actual - 1, columna_actual) not in muro and (
+            fila_actual - 1, columna_actual) not in visitados:
+        pila.append((fila_actual - 1, columna_actual, camino + ['Arriba']))
+
+    if columna_actual > 0 and (fila_actual, columna_actual - 1) not in muro and (
+            fila_actual, columna_actual - 1) not in visitados:
+        pila.append((fila_actual, columna_actual - 1, camino + ['Izquierda']))
+
+    return None
+
+
+pila = [(0, 0, [])]
+visitados = set()
+
+while len(pila) > 0:
+    fila_actual, columna_actual, camino = pila.pop()
+    if (fila_actual, columna_actual) not in visitados:
+        visitados.add((fila_actual, columna_actual))
+        resultado = resolver_laberinto(fila_actual, columna_actual, camino, visitados)
+        if resultado is not None:
+            print(resultado)
+            break
+
 for fila in laberinto:
     print(' '.join(fila))
